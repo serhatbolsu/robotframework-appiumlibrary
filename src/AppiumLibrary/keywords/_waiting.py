f6e552041d939a92c90c5a490102ac8d2c2b19d4
dungb <mrbazen@gmail.com>
2015-06-29 12:04:21 +0300

    Added the keywords: Wait Until Page Does Not Contain, Wait Until Page Contains Element, Wait Until Page Does Not Contain Element. Updated the doc description for keyword Wait Until Page Contain

--- src/AppiumLibrary/keywords/_waiting.py
+++ src/AppiumLibrary/keywords/_waiting.py
@@ -14,11 +14,76 @@ class _WaitingKeywords(KeywordGroup):
 
         `error` can be used to override the default error message.
 
-        See also BuiltIn keyword `Wait Until Keyword Succeeds`.
+        See also `Wait Until Page Does Not Contain`, 
+        `Wait Until Page Contains Element`,
+        `Wait Until Page Does Not Contain Element` and  
+        BuiltIn keyword `Wait Until Keyword Succeeds`.
         """
         if not error:
             error = "Text '%s' did not appear in <TIMEOUT>" % text
         self._wait_until(timeout, error, self._is_text_present, text)
+
+    def wait_until_page_does_not_contain(self, text, timeout=None, error=None):
+        """Waits until `text` disappears from current page.
+
+        Fails if `timeout` expires before the `text` disappears. See
+        `introduction` for more information about `timeout` and its
+        default value.
+
+        `error` can be used to override the default error message.
+
+        See also `Wait Until Page Contains`, 
+        `Wait Until Page Contains Element`,
+        `Wait Until Page Does Not Contain Element` and 
+        BuiltIn keyword `Wait Until Keyword Succeeds`.
+        """
+        def check_present():
+            present = self._is_text_present(text)
+            if not present:
+                return
+            else:
+                return error or "Text '%s' did not disappear in %s" % (text, self._format_timeout(timeout))
+        self._wait_until_no_error(timeout, check_present)
+		
+    def wait_until_page_contains_element(self, locator, timeout=None, error=None):
+        """Waits until element specified with `locator` appears on current page.
+
+        Fails if `timeout` expires before the element appears. See
+        `introduction` for more information about `timeout` and its
+        default value.
+
+        `error` can be used to override the default error message.
+
+        See also `Wait Until Page Contains`, 
+        `Wait Until Page Does Not Contain`
+        `Wait Until Page Does Not Contain Element` 
+        and BuiltIn keyword `Wait Until Keyword Succeeds`.
+        """
+        if not error:
+            error = "Element '%s' did not appear in <TIMEOUT>" % locator
+        self._wait_until(timeout, error, self._is_element_present, locator)
+
+    def wait_until_page_does_not_contain_element(self, locator, timeout=None, error=None):
+        """Waits until element specified with `locator` disappears from current page.
+
+        Fails if `timeout` expires before the element disappears. See
+        `introduction` for more information about `timeout` and its
+        default value.
+
+        `error` can be used to override the default error message.
+
+        See also `Wait Until Page Contains`, 
+        `Wait Until Page Does Not Contain`, 
+        `Wait Until Page Contains Element` and 
+        BuiltIn keyword `Wait Until Keyword Succeeds`.
+        """
+        def check_present():
+            present = self._is_element_present(locator)
+            if not present:
+                return
+            else:
+                return error or "Element '%s' did not disappear in %s" % (locator, self._format_timeout(timeout))
+        self._wait_until_no_error(timeout, check_present)
         
     # Private
 
