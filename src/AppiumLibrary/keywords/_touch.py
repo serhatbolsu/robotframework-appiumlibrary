ca3eaf1f4bebd593297f66fc60629ccfd58079b7
Minh Nguyen <minhnguyenphuonghoang@gmail.com>
2015-09-26 16:24:10 +0700

    Update _touch.py
    Add keyword 'click a point' to click on a specific point using given opt(x,y)
    Raise exception error if appium can't perform click on that point.

--- src/AppiumLibrary/keywords/_touch.py
+++ src/AppiumLibrary/keywords/_touch.py
@@ -58,3 +58,13 @@ class _TouchKeywords(KeywordGroup):
         el = self._element_find(locator, True, True)
         action = TouchAction(driver)
         action.tap(el).perform()
+        
+    def click_a_point(self, x=0, y=0):
+        """ Click on a point"""
+        self._info("Clicking on a point (%s,%s)." % (x,y))
+        driver = self._current_application()
+        action = TouchAction(driver)
+        try:
+            action.press(x=float(x), y=float(y)).perform()
+        except:
+            assert False, "Can't click on a point at (%s,%s)" % (x,y)
