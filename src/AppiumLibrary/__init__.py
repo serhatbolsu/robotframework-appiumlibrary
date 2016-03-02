900eca88c5343f617807c35099fed16a366f6d30
Murugu Venkat <mvenkat@ziprealty.com>
2016-03-02 12:08:45 -0800

    Fixed errors in documentation with respect to the correct ways to call Android's UI Automator

--- src/AppiumLibrary/__init__.py
+++ src/AppiumLibrary/__init__.py
@@ -43,7 +43,7 @@ class AppiumLibrary(
     | xpath             | Click Element `|` xpath=//UIATableView/UIATableCell/UIAButton  | Matches with arbitrary XPath      |
     | class             | Click Element `|` class=UIAPickerWheel                         | Matches by class                  |
     | accessibility_id  | Click Element `|` accessibility_id=t                           | Accessibility options utilize.    |
-    | android           | Click Element `|` android=new UiSelector().description('Apps') | Matches by Android UI Automator   |
+    | android           | Click Element `|` android=UiSelector().description('Apps')     | Matches by Android UI Automator   |
     | ios               | Click Element `|` ios=.buttons().withName('Apps')              | Matches by iOS UI Automation      |
     | css               | Click Element `|` css=.green_button                            | Matches by css in webview         |
 
