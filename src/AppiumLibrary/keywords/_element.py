ec176cd3e7d0b57997bdb3884f8c77d69c3d767b
Dhia Eddine Saidi <dhiasaidi.ensit@gmail.com>
2016-03-17 17:09:29 +0100

    Update _element.py

--- src/AppiumLibrary/keywords/_element.py
+++ src/AppiumLibrary/keywords/_element.py
@@ -413,3 +413,15 @@ class _ElementKeywords(KeywordGroup):
         application = self._current_application()
         elements = self._element_finder.find(application, locator, None)
         return len(elements) > 0
+    def get_text(self, locator):
+        """Returns the text value of element identified by `locator`.
+        See `introduction` for details about locating elements.
+        """
+        return self._get_text(locator)
+        
+    def _get_text(self, locator):
+        element = self._element_find(locator, True, True)
+        if element is not None:
+            return element.text
+        return None
+
