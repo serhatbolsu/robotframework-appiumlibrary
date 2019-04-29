serhatbolsu/robotframework-appiumlibrary

commit 9f430aca5f2932cf201f026f85c7d9353f3a3f82

Fixed #238 `Get Element Attribute` can not be used with `Get Webelements`

AppiumLibrary/keywords/_element.py
@@ -561,6 +561,7 @@ class _ElementKeywords(KeywordGroup):
 
     def _element_find(self, locator, first_only, required, tag=None):
         application = self._current_application()
+        elements = None
         if isstr(locator):
             _locator = locator
             elements = self._element_finder.find(application, _locator, tag)
@@ -570,7 +571,7 @@ class _ElementKeywords(KeywordGroup):
                 if len(elements) == 0: return None
                 return elements[0]
         elif isinstance(locator, WebElement):
-            elements = locator
+            elements = [locator]
         # do some other stuff here like deal with list of webelements
         # ... or raise locator/element specific error if required
         return elements

commit 885df74572dad7575e7b0db6d06df602137aa7c4

Fix the click element if locator is a WebElement instance
_element_find function should return an element if caller need an element
This function was broken by the commit 9f430aca5f2932cf201f026f85c7d9353f3a3f82

AppiumLibrary/keywords/_element.py
@@ -571,7 +571,10 @@ class _ElementKeywords(KeywordGroup):
                 if len(elements) == 0: return None
                 return elements[0]
         elif isinstance(locator, WebElement):
-            elements = [locator]
+            if first_only:
+                return elements[0]
+            else:
+                elements = [locator]
         # do some other stuff here like deal with list of webelements
         # ... or raise locator/element specific error if required
         return elements