90dba58e636ab1208450716af004c95cdf528f4b
alpha <alpha.com>
2024-05-21 11:29:42 +0200

    selenium compatibility with appium-client

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ setup(name='robotframework-appiumlibrary',
           'decorator >= 3.3.2',
           'robotframework >= 2.6.0',
           'docutils >= 0.8.1',
-          'selenium >=4.0,<=4.9',
+          'selenium >=4.12.0',
           'Appium-Python-Client == 3.2.1',
           'kitchen >= 1.2.4',
           'six >= 1.10.0'
